ae7286c00cbe6298c5a92e46bd66b6199ffae0f0
Anubhab Das <Anubhab.Das@Lnttechservices.com>
2018-12-04 15:05:59 +0530

    First commit

---
+++ app/src/main/java/com/tokbox/android/accelerator/sample/config/OpenTokConfig.java
@@ -0,0 +1,14 @@
+package com.tokbox.android.accelerator.sample.config;
+
+public class OpenTokConfig {
+
+    // *** Fill the following variables using your own Project info from the OpenTok dashboard  ***
+    // ***                      https://dashboard.tokbox.com/projects                           ***
+    public static final String SESSION_ID = "1_MX40NjIyNDAyMn5-MTU0MzI5NzI4ODg2OH4rMUdsSmlNcHd1czV3Z2VVSUNkeGdzODV-fg";
+    // Replace with a generated token (from the dashboard or using an OpenTok server SDK)
+    public static final String TOKEN = "T1==cGFydG5lcl9pZD00NjIyNDAyMiZzaWc9MDdiODgyMTczYTQ3NGY4OWFhNzUzMGVlNmYwN2JhMDlhYmE5OWRiMTpzZXNzaW9uX2lkPTFfTVg0ME5qSXlOREF5TW41LU1UVTBNekk1TnpJNE9EZzJPSDRyTVVkc1NtbE5jSGQxY3pWM1oyVlZTVU5rZUdkek9EVi1mZyZjcmVhdGVfdGltZT0xNTQzMjk3MzA3Jm5vbmNlPTAuOTg3MzU4MzE3NTUzMzE2JnJvbGU9cHVibGlzaGVyJmV4cGlyZV90aW1lPTE1NDU4ODkyOTEmaW5pdGlhbF9sYXlvdXRfY2xhc3NfbGlzdD0=";
+    // Replace with your OpenTok API key
+    public static final String API_KEY = "46224022";
+
+
+}
